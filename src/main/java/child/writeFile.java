aaf786c03a210abc5f8d811f58a7242f9b38b2d4
Broaj Ghosh <broja.ghosh@gmail.com>
2024-01-16 17:30:29 +0530

    adding the logic to create a

---
+++ src/main/java/child/writeFile.java
@@ -0,0 +1,41 @@
+package child;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class writeFile {
+	public void writefile(String str) {
+		String path= "C:\\Works\\Java_demo\\child-util\\target\\file.txt";
+//		"..\\target/file.txt"
+		File file = new File(path);
+	    try {
+	      boolean value = file.createNewFile();
+	      if (value) {
+	        System.out.println("New Java File is created.");
+	      }
+	      else {
+	        System.out.println("The file already exists.");
+	      }
+	    }
+	    catch(Exception e) {
+	      e.getStackTrace();
+	    }
+	  
+	 try {
+	       // Creates a Writer using FileWriter
+	       FileWriter output = new FileWriter(path);
+
+	       // Writes the program to file
+	       output.write(str);
+	       System.out.println("Data is written to the file.");
+
+	       // Closes the writer
+	       output.close();
+	     }
+	     catch (Exception e) {
+	       e.getStackTrace();
+	     }
+	  }
+
+}
--- src/test/java/testCase/Testchild.java
+++ src/test/java/testCase/Testchild.java
@@ -3,6 +3,7 @@ package testCase;
 import org.testng.annotations.Test;
 
 import child.child;
+import child.writeFile;
 
 public class Testchild {
 
@@ -10,6 +11,8 @@ public class Testchild {
 public void testingfunction() {
 	child c=new child();
 	c.check("Ram");
+	writeFile wr=new writeFile();
+	wr.writefile("Ram");
 }
 
 
