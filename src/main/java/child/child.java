8adc36e4b49b1048bed1cee6413035cd0e2d40ae
Broaj Ghosh <broja.ghosh@gmail.com>
2024-01-16 10:14:31 +0530

    initial code

---
+++ src/main/java/child/child.java
@@ -0,0 +1,14 @@
+package child;
+
+import coreUtil.MasterUtil;
+
+public class child {
+	
+	public void check (String str) {
+		MasterUtil mst=new MasterUtil();
+		mst.baseMethod();
+		mst.baseMethodAdvnce(str);
+	}
+	
+
+}
---
+++ src/test/java/testCase/Testchild.java
@@ -0,0 +1,16 @@
+package testCase;
+
+import org.testng.annotations.Test;
+
+import child.child;
+
+public class Testchild {
+
+	@Test
+public void testingfunction() {
+	child c=new child();
+	c.check("Ram");
+}
+
+
+}
